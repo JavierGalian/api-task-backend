445319e97d31f8eaa68ea8106a2476cfb45bcee7
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2023-05-31 18:28:55 -0300

    arreglando bugs

--- apiTasks/settings.py
+++ apiTasks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'drf_yasg',
     'rest_framework',
     'tasks',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCESS_CONTROL_ALLOW_ORIGIN = ['http://127.0.0.1:5173',] 
+#cors authorization
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173',]
--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from.views import TaskAPIView, TaskDetailAPIView
 urlpatterns = [
     path('task/', TaskAPIView.as_view(), name='tasks'),
-    path('task/<int:pk>', TaskDetailAPIView.as_view(), name= 'task-detail')
+    path('task/<int:pk>/', TaskDetailAPIView.as_view(), name= 'task-detail')
 ]
