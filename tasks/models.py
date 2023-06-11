42f16a09230877184bffc51be4786faf75731796
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2023-06-11 17:21:14 -0300

    project api task backend

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class TaskModel(models.Model):
+    id = models.AutoField(primary_key=True)
     task = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
